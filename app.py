2d8075da531f40880f58aaabc597489e4b2aefb0
Dayluf <145565950+Dayluf@users.noreply.github.com>
2023-12-05 12:44:41 +0100

    python, server-side processing
    lagde en python fil med kode fra chatgpt, lagde en static folder hvor jeg la sign up css og lagde en templates hvor jeg la sign up html

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    # Retrieve form data
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+# Basic validation (you can add more complex validation)
+    if not username or not email or not password:
+        return "All fields are required. Please go back and fill out the form."
+
+# If validation passes, you might store the data in a database
+    # For simplicity, let's just print the data for now
+    print(f"Username: {username}, Email: {email}, Password: {password}")
+
+    return "Signup successful!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
