8d4f35190f72442ddfef29631ea9accdbc50f236
Dayluf <145565950+Dayluf@users.noreply.github.com>
2023-12-11 08:42:17 +0100

    endret masse
    byttet navn på website.html filen min til index, la til en get i app route methods, la til en if med post, og endret request.form.get til bare .form. stylet noen forskjellige ting i style css. la til en signupinfo.html side som vil gi brukeren informasjon om hva som har blitt registrert.

--- app.py
+++ app.py
@@ -4,26 +4,28 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('webiste.html')
+    return render_template('index.html')
 
-
-
-@app.route('/signup', methods=['POST'])
+@app.route('/signup', methods=['GET','POST'])
 def signup():
+    if request.method == 'POST':
     # Retrieve form data
-    username = request.form.get('username')
-    email = request.form.get('email')
-    password = request.form.get('password')
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
 
-# Basic validation (you can add more complex validation)
-    if not username or not email or not password:
-        return "All fields are required. Please go back and fill out the form."
+    # Basic validation (you can add more complex validation)
+        if not username or not email or not password:
+            return "All fields are required. Please go back and fill out the form."
 
-# If validation passes, you might store the data in a database
-    # For simplicity, let's just print the data for now
-    print(f"Username: {username}, Email: {email}, Password: {password}")
+    # If validation passes, you might store the data in a database
+        # For simplicity, let's just print the data for now
+        print(f"Username: {username}, Email: {email}, Password: {password}")
 
-    return "Signup successful!"
+        # Render a page displaying the entered information
+        return render_template('signup_info.html', username=username, email=email, password=password)
+
+    return render_template('signup_form.html')
 
 if __name__ == '__main__':
     app.run(debug='True', port=100, host='0.0.0.0')
